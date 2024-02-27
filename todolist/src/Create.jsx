55ea720c92b0f089a414c2a9f8f645d9c86a5b84
bishalbhatta1028 <bishalbhatta1028@gamil.com>
2024-02-27 17:57:27 +0545

    change

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import axios from "axios"
+import { useState } from 'react'
+import axios from 'axios'
 import("./App.css")
 export default function () {
     const [task, setTask] = useState()
     const handleAdd = () => {
-        axios.post({}).then(result => console.log(result))
-            .catch(err => console.log(err))
 
+        axios.post(" http://localhost:8000/add", { task: task })
+            .then(result => console.log(result))
+            .catch(err => console.log(err))
 
     }
     return (
         <div className="input">
-            <input type="text" name="" className="input" id="" placeholder='Enter the Task' />
+            <input type="text" name="" className="input" id="" placeholder='Enter the Task' onChange={(e) => setTask(e.target.value)} />
             <button type="button" className="button" onClick={handleAdd}>Add</button>
         </div>
     )
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,19 +1,49 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Create from './Create'
 import('./App.css')
 function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() => {
+        axios.get(" http://localhost:8000/add").then(result => setTodos(result.data))
+            .catch(err => console.log(err))
+    }, [todos])
     return (
-        <div>
-            <h2 className="home"> Todo Application</h2>
+        <>
+            {/* <h2 className="home"> Todo Application</h2>
             <Create />
             {todos.length === 0 ?
                 <div ><h2 className="first">No Records</h2></div> :
-                todos.map(todo => {
-                    <div>{todos} </div >
-                })
-            }</div>
+
+                <div> {todos.map((task) => (
+                    <div className="table" >
+                        <table className="tabledata" >
+                            <tr key={task._id} className="data"> {task.task}</tr>
+                        </table>
+                    </div>
+                ))}  </div >
+            } */}
+
+            <h2 className="home"> Todo Application</h2>
+            <Create />
+
+            {todos.length === 0 ? (
+                <div><h2 className="first">No Records</h2></div>
+            ) : (
+                <div>
+                    <table className="tabledata">
+                        <tbody>
+                            {todos.map((task) => (
+                                <tr key={task._id} className="data">
+                                    <td>{task.task}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+        </>
     )
 }
 
